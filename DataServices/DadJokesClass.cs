399c1e11a170ce5caf077d497ab0b26ca5f97d40
kr-ambuj <kumar.ambuj@hotmail.com>
2023-03-21 12:03:41 +0530

    Add project files.

---
+++ DataServices/DadJokesClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JokesApp.DataServices
+{
+    internal class DadJokesClass
+    {
+        string strJokeUrl = string.Empty;
+        public DadJokesClass()
+        {
+            strJokeUrl = System.Windows.Application.Current.FindResource("strUrl").ToString();
+        }
+
+        internal async Task<string> GetARandomJoke()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, strJokeUrl);
+            request.Headers.Add("User-Agent", "");
+            request.Headers.Add("Accept", " application/json");
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using JokesApp.DataServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JokesApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DadJokesClass DadJokes;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DadJokes = new DadJokesClass();
+        }
+
+        private void _btnGetRandomJoke_Click(object sender, RoutedEventArgs e)
+        {
+            _txtDisplayJoke.Text = DadJokes.GetARandomJoke().Result;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
